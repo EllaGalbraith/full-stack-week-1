75158fb374cd3957508c57c53f2bb9c1c746edda
Ella Galbraith <ellagalbraithh@gmail.com>
2025-01-20 20:57:45 -0700

    week 02 assignment done

---
+++ src/app/contacts/contact-details/contact-details.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+import { Contact } from '../contact.model';
+
+@Component({
+  selector: 'app-contact-details',
+  standalone: false,
+  
+  templateUrl: './contact-details.component.html',
+  styleUrl: './contact-details.component.css'
+})
+export class ContactDetailsComponent {
+  detail: Contact = new Contact(1, "R. Kent Jackson", "jacksonk@byui.edu", "208-496-3771",  "/images/jacksonk.jpg", null);
+  
+
+}
